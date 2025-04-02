2c3fea4fc8507066491f28ca90f56f0ccf7a3c6d
Rafael Santos <rafaelcachorroquentefoda@gmail.com>
2025-04-01 23:36:44 -0300

    tiposvariaveis aula 5

--- meu-primeiro-projetoJava/src/edu/rafael/tiposvariaveis/TiposVariaveis.java
+++ meu-primeiro-projetoJava/src/edu/rafael/tiposvariaveis/TiposVariaveis.java
@@ -1,5 +1,12 @@
 package edu.rafael.tiposvariaveis;
 
 public class TiposVariaveis {
-    
+    double salarioMinimo = 2500.33;
+    byte idade = 123;
+    short ano = 2021;
+    int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+    long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+    float pi = 3.14F;
+
+
 }
